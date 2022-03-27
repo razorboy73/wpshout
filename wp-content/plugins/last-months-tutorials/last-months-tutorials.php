0582893cce7a5f6c78b671e4068b5cb950a02fbc
Josh Kerbel <joshkerbel@MacBook-Pro.local>
2022-03-27 09:07:37 -0400

    created class for plugin

--- wp-content/plugins/last-months-tutorials/last-months-tutorials.php
+++ wp-content/plugins/last-months-tutorials/last-months-tutorials.php
@@ -16,90 +16,82 @@ License: GPL2
 // with clickable links.
 
 
+class LastMonthsTutorials{
+    
+    function __construct() {
+        //provides shortcode functionality        
+        add_shortcode("lmt",array($this,"lmt_display_shortcode"));
+        //adds the display to the content
+        add_filter( 'the_content', array($this,'lmt_display_shortcode') );
+        // adds to settings menu
+        add_action("admin_menu", array($this,"adminPage"));
+    
+    }
 
-
-
-
-// function last_months_tutorial($content){
-//     if ( ! is_singular( 'tutorial' ) ) {
-// 		return $content;
-	
-//     $q_args = array(
-//         "post_type" => "tutorial"
-//     );
-
-//     $query = new WP_Query($q_args);
-
-//     if($query->have_posts()){
-//         while ($query->have_posts()){
-//             $query->the_post();
-//             $content = the_title(). $content;
-//         }
-//     }
-  
-
-   
-//     }
-//     wp_reset_postdata();
-//     return $content;
-
-// }
-
-
-
-// add_filter( 'the_content', 'last_months_tutorial' );
-
-add_shortcode("lmt","lmt_display_shortcode");
-
-function lmt_display_shortcode($attributes){
-    $attributes = shortcode_atts( array( 
-		'difficulty' => 'advanced',
-	), $attributes, 'lmt_display_shortcode' );
-
-   
-	
-    $q_args = array(
-        "post_type" => "tutorial",
-        "posts_per_page" => -1,
-        'date_query' => array(
-	        array(
-                //figure out last momnth
-	            'after'     => date("F j y", strtotime("first day of previous month")),
-                'before'     => date("F j y", strtotime("first day of this month")),
-	            'inclusive' => false,
-	        )
-            ),
-            'tax_query' => array(
+    function lmt_display_shortcode($attributes){
+        $attributes = shortcode_atts( array( 
+            'difficulty' => 'beginner',
+        ), $attributes, 'lmt_display_shortcode' );
+    
+    
+        $q_args = array(
+            "post_type" => "tutorial",
+            "posts_per_page" => -1,
+            'date_query' => array(
                 array(
-                    'taxonomy' => 'difficulty',
-                    'field'    => 'slug',
-                    'terms'    => $attributes['difficulty']
+                    //figure out last momnth
+                    'after'     => date("F j y", strtotime("first day of previous month")),
+                    'before'     => date("F j y", strtotime("first day of this month")),
+                    'inclusive' => false,
+                )
                 ),
-            )
-
-   
-    );
-
-    $query = new WP_Query($q_args);
-
+                'tax_query' => array(
+                    array(
+                        'taxonomy' => 'difficulty',
+                        'field'    => 'slug',
+                        'terms'    => $attributes['difficulty']
+                    ),
+                ),
     
-    if( $query->have_posts() ) :
-        echo '<div class="last-month-tuts">';
-		echo '<h3>'.ucfirst($attributes['difficulty']).' Tutorials From Last Month</h3> <ul>';
-		while( $query->have_posts() ) :
-			$query->the_post();
-			echo '<li><a href="' . get_the_permalink() . '">' . get_the_title() . '</a>.  Published on: '. get_the_date().'</li>';
-		endwhile;
-		echo '</ul></div>';
-	endif;
-  
-
-   
+       
+        );
     
-    wp_reset_postdata();
-
+        $query = new WP_Query($q_args);
     
-
+        $buffer = " ";
+        if( $query->have_posts() ) :
+            $buffer = $buffer. '<div class="last-month-tuts">';
+            $buffer = $buffer. '<h3>'.ucfirst($attributes['difficulty']).' Tutorials From Last Month</h3> <ul>';
+            while( $query->have_posts() ) :
+                $query->the_post();
+                $buffer = $buffer.  '<li><a href="' . get_the_permalink() . '">' . get_the_title() . '</a>.  Published on: '. get_the_date().'</li>';
+            endwhile;
+            $buffer = $buffer. '</ul></div>';
+        endif;
+      
     
-  
+       
+        
+        wp_reset_postdata();
+    
+        return $buffer;
+    }
+    
+    
+    
+    function adminPage(){
+        add_options_page("Last Month's Tutorial Display Settings", "Tutorial Display",
+        "manage_options","lmt-settings-page", array($this,"lmtHTML") );
+    }
+    
+    function lmtHTML(){ ?>
+    Hello from plugin settings page
+    <?php }
+
+
 }
+
+$lastMonthsTutorials = new LastMonthsTutorials();
+
+
+
